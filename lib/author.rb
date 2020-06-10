5724bbe0ba79bfb7c453e8823a0ca299c87f1902
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-10 14:06:47 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,35 @@
+class Author
+
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts_array = []
+  end
+
+  def posts
+    Post.all.select{|p| p.author == self}
+  end
+
+  def add_post(title)
+
+    title.author = self
+    @@post_count += 1
+    @posts_array << title
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+
+    post.author = self
+    @@post_count +=1
+    @posts_array << post
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
