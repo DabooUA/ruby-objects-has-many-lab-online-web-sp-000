e449d794eb2c10990b9ee2695ec8b24681f4f1df
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-10 14:41:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,11 +2,11 @@ class Author
 
   attr_accessor :name
 
-  @@post_count = 0
+  #@@post_count = 0
 
   def initialize(name)
     @name = name
-    @posts_array = []
+    #@posts_array = []
   end
 
   def posts
@@ -14,22 +14,21 @@ class Author
   end
 
   def add_post(title)
-
     title.author = self
-    @@post_count += 1
-    @posts_array << title
+    #@@post_count += 1
+    #@posts_array << title
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
 
     post.author = self
-    @@post_count +=1
-    @posts_array << post
+    #@@post_count +=1
+    #@posts_array << post
   end
 
   def self.post_count
-    @@post_count
+    Post.all.count
   end
 
 end
